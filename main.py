d4c8feb83f9efffbaf485ee584a1d6d239fa83c7
Joe Zator <jmzator@gmail.com>
2023-07-25 13:07:22 -0600

    Creates matplotlib py file, deletes PIL code from main, amends gitignore to ignore sample images folder

--- main.py
+++ main.py
@@ -1,63 +1,6 @@
 
 from coloring import histogram_equalize_detector_image
-
-
-
-########
-# this code fills in dummy images into printable grid
-# can change the 'square_size' and 'grid_size' parameter below
-# to get different number of globes of varying sizes on the sheet
-# based upon meeting with Nick & Kyle on 2023-07-17 with different
-# size printouts, for now targeting the size that fits 4 columns
-# and six rows of globes on the 8.5 x 11 printout (which would be
-
-# square_size = 15 and grid_size = 6, 4
-
-# will need to see how that looks on the 42 inch paper rolls then
-# after get approval with some actual globe images
-
-from PIL import Image, ImageDraw
-
-square_size = 15  # cm
-grid_size = (6, 4)  # number of squares in the grid (rows, columns)
-margin = 0.1  # cm
-output_file = "grid.png"
-
-# image size in pixels
-image_width = int((grid_size[0] * square_size + (grid_size[0] + 1) * margin) * 37.7952755906)
-image_height = int((grid_size[1] * square_size + (grid_size[1] + 1) * margin) * 37.7952755906)
-
-# blank image
-image = Image.new("RGB", (image_width, image_height), "white")
-draw = ImageDraw.Draw(image)
-
-# add grid lines
-for i in range(grid_size[0] + 1):
-    x = i * (square_size + margin) * 37.7952755906 + margin * 37.7952755906
-    draw.line([(x, margin * 37.7952755906), (x, image_height - margin * 37.7952755906)], fill="black")
-
-for j in range(grid_size[1] + 1):
-    y = j * (square_size + margin) * 37.7952755906 + margin * 37.7952755906
-    draw.line([(margin * 37.7952755906, y), (image_width - margin * 37.7952755906, y)], fill="black")
-
-# add png dummy file for testing printed size for viewing
-for i in range(grid_size[0]):
-    for j in range(grid_size[1]):
-        # calculate position of square in pixels
-        x = (i + 1) * margin * 37.7952755906 + i * square_size * 37.7952755906
-        y = (j + 1) * margin * 37.7952755906 + j * square_size * 37.7952755906
-
-        png_file = Image.open("/Users/jmzator/Desktop/LASP_Maven_job/globe_sample_images/dummy_images_from_kyle/orbit03453-Ls1820-angle0410-binning0133x0019-heq-globe.png")
-
-        # resize to fit in the square
-        png_file.thumbnail((square_size * 37.7952755906, square_size * 37.7952755906))
-        image.paste(png_file, (int(x), int(y)))
-
-# save final image
-image.save(output_file)
-
-#### end that part ####
-
+from matplotlib_dummy_grid import plot_images_on_grid
 
 
 ###########
@@ -88,8 +31,10 @@ raise SystemExit(9)
 print(np.shape(data))
 
 
-###### end that part ######
+###### end ######
 
 
 
 # histogram fxn next, need to troubleshoot the import at top
+# try import of matplotlib_dummy_grid.py function that working on too
+
---
+++ matplotlib_dummy_grid.py
@@ -0,0 +1,50 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_images_on_grid(image_dir, image_files, output_file):
+    # number of rows and columns in grid
+    num_rows = 3
+    num_cols = 3
+
+    # figure size for square grid
+    fig_width = 9.0  # cm
+    fig_height = 9.0
+    dpi = 300  # dpi resolution
+
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(fig_width, fig_height), dpi=dpi)
+
+    for i, ax in enumerate(axs.flat):
+        if i < len(image_files):
+            image_path = os.path.join(image_dir, image_files[i])
+            img = plt.imread(image_path)
+            ax.imshow(img)
+            ax.axis('off')
+        else:
+            ax.set_visible(False)
+
+    plt.tight_layout()
+    plt.savefig(output_file)
+    plt.close()
+
+if __name__ == "__main__":
+    image_directory = "/Users/jmzator/Desktop/LASP_Maven_job/globe_sample_images/dummy_images_from_kyle/"
+    image_files = [
+        "orbit03453-Ls1820-angle0410-binning0133x0019-heq-globe.png",
+        "orbit03475-Ls1843-angle0345-binning0133x0019-heq-globe.png",
+        "orbit03453-Ls1820-angle0410-binning0133x0019-heq-globe.png",
+        "orbit03475-Ls1843-angle0345-binning0133x0019-heq-globe.png",
+        "orbit03453-Ls1820-angle0410-binning0133x0019-heq-globe.png",
+        "orbit03475-Ls1843-angle0345-binning0133x0019-heq-globe.png",
+        "orbit03453-Ls1820-angle0410-binning0133x0019-heq-globe.png",
+        "orbit03475-Ls1843-angle0345-binning0133x0019-heq-globe.png",
+        "orbit03453-Ls1820-angle0410-binning0133x0019-heq-globe.png",
+
+        # above is just two dummy images over and over again
+        # add more then, for final project will need code to add all as created
+    ]
+    output_file = "sample_grid.png"
+    plot_images_on_grid(image_directory, image_files, output_file)
+
+
+### end ###
