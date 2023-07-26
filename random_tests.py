082ff06b0638b72ae687f313f6dba91ac58ed467
Joe Zator <jmzator@gmail.com>
2023-07-26 14:29:33 -0600

    Adds random_tests py file as dump point and adds .npy to gitignore for saved arrays output

---
+++ random_tests.py
@@ -0,0 +1,11 @@
+
+# dump point for testing random things in project
+# keep separate from main or necessary python files
+
+import numpy as np
+
+data = np.load('iuvs_test_array.npy')
+print(data)
+
+
+### end ###
