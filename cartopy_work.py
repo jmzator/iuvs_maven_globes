3347ab751669dd035e22a311ddcc1381aa469fa8
Joe Zator <jmzator@gmail.com>
2023-08-01 13:05:49 -0600

    Attempt reshape of test array for globe map in cartopy_work

--- cartopy_work.py
+++ cartopy_work.py
@@ -8,9 +8,10 @@ import numpy as np
 # example says need 2d data array (like 180 lat and 360 lon for globe)
 lat = np.linspace(-90, 90, 180)
 lon = np.linspace(-180, 180, 360)
-#data = np.random.rand(180, 360)
+#data = np.random.rand(180, 360)  this was just for random trial
 data = np.load('iuvs_test_array.npy')
-data_bin = data[:, :, 0]
+data_bin = np.reshape(data, (data.shape[0] * data.shape[1], data.shape[2]))
+#data_bin = data[:, :, 0]  this was for leaving off last array shape since cartopy wants 2 not 3
 lats = np.linspace(-90, 90, data.shape[0])
 lons = np.linspace(0, 360, data.shape[1])
 
@@ -23,8 +24,8 @@ fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.Orthographic(central_longitude=0, central_latitude=0))
 ax.set_global()
 
-# plot the 2d data array
-img = ax.pcolormesh(lon2d, lat2d, data_bin, transform=ccrs.PlateCarree(), cmap='viridis')
+# plot the 2d data array (still need to make sure can add 3rd as the RGB
+img = ax.pcolormesh(lon2d, lat2d, data_bin, transform=ccrs.PlateCarree()) #, cmap='viridis')
 
 # plt.show()
 
